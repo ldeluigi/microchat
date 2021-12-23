88c6707e4bbfab9144cb19ca9e5c6f48b4525bc9
ThomasAngeliniUnibo <thomas.angelini@studio.unibo.it>
2021-12-23 15:03:38 +0100

    create entity for chat context

---
+++ services/chat/src/ChatService.Domain/User.cs
@@ -0,0 +1,25 @@
+using System;
+using EasyDesk.CleanArchitecture.Domain.Metamodel;
+
+namespace Microchat.Domain.Aggregates.Message
+{
+    /// <summary>
+    /// User entity.
+    /// </summary>
+    public class User : Entity
+    {
+        /// <summary>
+        /// Creation of an User.
+        /// </summary>
+        /// <param name="id">Identificator of the User.</param>
+        public User(Guid id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// Identificator of this User.
+        /// </summary>
+        public Guid Id { get; }
+    }
+}
