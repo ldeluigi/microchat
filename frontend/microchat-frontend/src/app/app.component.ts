ac08d956bdc696b2230b3fb0f25f79daea4afb09
Simone <Simone.magnani4@studio.unibo.it>
2021-12-26 12:46:26 +0100

    Various update on frontend

--- frontend/microchat-frontend/src/app/app.component.ts
+++ frontend/microchat-frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import * as $ from 'jquery';
 import { Message } from 'src/model/Message';
 
@@ -10,15 +10,35 @@ import { Message } from 'src/model/Message';
 export class AppComponent implements OnInit {
   title = 'microchat-frontend';
   messages: Message[] = []; // messages
+  //@ViewChild('TextArea') newMessage!: TextArea
 
   ngOnInit() {
-    this.messages.push({id: "id",text:"", editText:(a)=>"id",sender:""});
+    this.messages.push({id: "id",text:"Buon Natale", sendTime: new Date(2021,11,25,12),edited:true,sender:"Simo"});
+    this.messages.push({id: "id",text:"Grazie, anche a te e famiglia!", sendTime: new Date(2021,11,25,12,1), edited:false,sender:"Thommy"});
+    this.messages.push({id: "id",text:":)", sendTime: new Date(2021,11,25,12,2), edited:false,sender:"Simo"});
+    this.messages.push({id: "id",text:"Dai ricominciamo a lavorare al proj", sendTime: new Date(2021,11,26,12), edited:true,sender:"Thommy"});
     $('#action_menu_btn').on("click", function(){
       $('.action_menu').toggle();
     });
   }
 
   getId(message: Message) {
-    return message.id ? "mine" : "other";
+    return this.amITheSender(message.sender) ? "mine" : "other";
+  }
+
+  imgFirst(message: Message) {
+    return this.amITheSender(message.sender);
+  }
+
+  private amITheSender(sender: string) {
+    return sender == "Simo"
+  }
+
+  getSrcImg(message: Message) {
+    return "https://therichpost.com/wp-content/uploads/2020/06/avatar2.png";
+  }
+
+  sendMessage() {
+    console.log($("#newMessage")[0]);
   }
 }
