d5a58d368a15da2af9c268e2e0b1c74b67e19bdd
Simone <Simone.magnani4@studio.unibo.it>
2021-12-26 15:45:18 +0100

    Create new chat componenct

--- frontend/microchat-frontend/src/app/app.component.ts
+++ frontend/microchat-frontend/src/app/app.component.ts
@@ -9,35 +9,12 @@ import { Message } from 'src/model/Message';
 })
 export class AppComponent implements OnInit {
   title = 'microchat-frontend';
-  messages: Message[] = []; // messages
-  //@ViewChild('TextArea') newMessage!: TextArea
 
   ngOnInit() {
-    this.messages.push({id: "id",text:"Buon Natale", sendTime: new Date(2021,11,25,12),edited:true,sender:"Simo"});
-    this.messages.push({id: "id",text:"Grazie, anche a te e famiglia!", sendTime: new Date(2021,11,25,12,1), edited:false,sender:"Thommy"});
-    this.messages.push({id: "id",text:":)", sendTime: new Date(2021,11,25,12,2), edited:false,sender:"Simo"});
-    this.messages.push({id: "id",text:"Dai ricominciamo a lavorare al proj", sendTime: new Date(2021,11,26,12), edited:true,sender:"Thommy"});
     $('#action_menu_btn').on("click", function(){
       $('.action_menu').toggle();
     });
   }
-
-  getId(message: Message) {
-    return this.amITheSender(message.sender) ? "mine" : "other";
-  }
-
-  imgFirst(message: Message) {
-    return this.amITheSender(message.sender);
-  }
-
-  private amITheSender(sender: string) {
-    return sender == "Simo"
-  }
-
-  getSrcImg(message: Message) {
-    return "https://therichpost.com/wp-content/uploads/2020/06/avatar2.png";
-  }
-
   sendMessage() {
     console.log($("#newMessage")[0]);
   }
