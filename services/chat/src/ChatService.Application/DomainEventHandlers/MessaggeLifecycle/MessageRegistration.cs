dc6b4e512ac71546e69a2b0c100d09d3c25f8498
ThomasAngeliniUnibo <thomas.angelini@studio.unibo.it>
2022-02-01 11:35:00 +0100

    Fix some minor error

--- services/chat/src/ChatService.Application/DomainEventHandlers/MessaggeLifecycle/MessageRegistration.cs
+++ services/chat/src/ChatService.Application/DomainEventHandlers/MessaggeLifecycle/MessageRegistration.cs
@@ -4,12 +4,13 @@ using EasyDesk.CleanArchitecture.Application.Mediator;
 using EasyDesk.CleanArchitecture.Application.Responses;
 using EasyDesk.CleanArchitecture.Domain.Metamodel;
 using EasyDesk.Tools;
+using EasyDesk.Tools.PrimitiveTypes.DateAndTime;
 using System;
 using System.Threading.Tasks;
 
 namespace ChatService.Application.DomainEventHandlers.MessaggeLifecycle;
 
-public record MessageCreated(uid Id, Guid ChatId, string Text, Guid Sender, Timestamp Sendtime) : DomainEvent;
+public record MessageCreated(Guid Id, Guid ChatId, string Text, Guid Sender, Timestamp Sendtime) : DomainEvent;
 
 public class MessageRegistration : DomainEventHandlerBase<MessageCreatedEvent>
 {
