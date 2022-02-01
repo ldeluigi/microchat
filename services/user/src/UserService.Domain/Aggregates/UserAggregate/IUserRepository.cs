5a2fa53de558e1aae082dd7ff78d57f45adce2ab
ThomasAngeliniUnibo <thomas.angelini@studio.unibo.it>
2022-02-01 12:13:44 +0100

    Update library for user

---
+++ services/user/src/UserService.Domain/Aggregates/UserAggregate/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using EasyDesk.CleanArchitecture.Domain.Metamodel.Repositories;
+using EasyDesk.CleanArchitecture.Domain.Metamodel.Results;
+using System;
+using System.Threading.Tasks;
+using UserService.Domain.Aggregates.UserAggregate;
+
+namespace UserService.Domain.Aggregates.UserAggregate;
+
+/// <summary>
+/// Repository for Message.
+/// </summary>
+public interface IUserRepository :
+    IGetByIdRepository<User, Guid>,
+    ISaveRepository<User>,
+    IRemoveRepository<User>
+{
+    Task<Result<User>> GetByEmail(Email email);
+}
---
+++ services/user/src/UserService.Domain/UserService/IRegistrationMethod.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using EasyDesk.CleanArchitecture.Domain.Metamodel.Results;
+using UserService.Domain.Aggregates.UserAggregate;
+
+namespace UserService.Domain.Aggregates.UserService;
+
+public interface IRegistrationMethod<T>
+{
+    Task<Result<User>> CreateUser(T userData);
+}
