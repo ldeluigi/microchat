6325ec4beb7f19db1f5dfa15b02baa62f6f2171f
ldeluigi <lucadelu97@gmail.com>
2022-02-05 14:13:57 +0100

    Add password changed endpoint

---
+++ services/auth/src/AuthService.Web/Controllers/V_1_0/Passwords/PasswordController.cs
@@ -0,0 +1,21 @@
+﻿using AuthService.Application.Commands.Passwords;
+using AuthService.Web.Controllers.V_1_0.Accounts.DTO.Outputs;
+using AuthService.Web.Controllers.V_1_0.Passwords.DTO;
+using EasyDesk.CleanArchitecture.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AuthService.Web.Controllers.V_1_0.Passwords;
+
+public class PasswordController : AbstractMediatrController
+{
+    [HttpPut(PasswordsRoutes.ChangePassword)]
+    public async Task<IActionResult> ChangePassword([FromRoute] Guid accountId, [FromBody] ChangePasswordBodyDto body)
+    {
+        var command = new ChangePassword.Command(body.OldPassword, body.NewPassword, accountId);
+        return await Command(command)
+            .MappingContent(Mapper.Map<AccountDto>)
+            .ReturnOk();
+    }
+}
---
+++ services/auth/src/AuthService.Web/Controllers/V_1_0/Passwords/PasswordsRoutes.cs
@@ -0,0 +1,6 @@
+﻿namespace AuthService.Web.Controllers.V_1_0.Passwords;
+
+public static class PasswordsRoutes
+{
+    public const string ChangePassword = "passwords/{accountId}";
+}
