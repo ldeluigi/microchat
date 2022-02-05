ddea93d760d5367ec342026196de0d4c0627af1f
Luca Deluigi <lucadelu97@gmail.com>
2022-02-05 12:33:32 +0100

    Draft SignalR  implementation

--- services/chat/src/ChatService.Web/Controllers/V_1_0/ChatSignalRController.cs
+++ services/chat/src/ChatService.Web/Controllers/V_1_0/ChatSignalRController.cs
@@ -7,8 +7,32 @@ namespace ChatService.Web.Controllers.V_1_0;
 
 public class ChatSignalRController : AbstractMediatrHub
 {
-    [HubMethodName("create-chat")]
-    public async Task CreateChat(Guid withId)
+    [HubMethodName("chat.createWith")]
+    public async Task CreateChatWith(Guid userId)
+    {
+
+    }
+
+    [HubMethodName("chat.delete")]
+    public async Task DeleteChat(Guid chatId)
+    {
+
+    }
+
+    [HubMethodName("message.send")]
+    public async Task SendMessage(Guid chatId, string text)
+    {
+
+    }
+
+    [HubMethodName("message.edit")]
+    public async Task EditMessage(Guid messageId, string text)
+    {
+
+    }
+
+    [HubMethodName("message.delete")]
+    public async Task DeleteMessage(Guid messageId)
     {
 
     }
