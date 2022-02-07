fc61299cd3bc64506619d80da18b16bd84192f70
ldeluigi <lucadelu97@gmail.com>
2022-02-07 15:54:35 +0100

    Draft SignalR Controller implementation with Groups

--- services/chat/src/ChatService.Web/Controllers/V_1_0/ChatSignalRController.cs
+++ services/chat/src/ChatService.Web/Controllers/V_1_0/ChatSignalRController.cs
@@ -1,58 +1,113 @@
-﻿using ChatService.Web.SignalR;
+﻿using ChatService.Application.Commands.PrivateChats;
+using ChatService.Application.Commands.PrivateMessages;
+using ChatService.Application.Queries.PrivateChats;
+using ChatService.Web.SignalR;
+using EasyDesk.CleanArchitecture.Application.Responses;
 using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ChatService.Web.Controllers.V_1_0;
 
 public class ChatSignalRController : AbstractMediatrHub
 {
-    public override Task OnConnectedAsync()
+    public override async Task OnConnectedAsync()
     {
-        // (getChatIds for Context.ConnectionId).ForEach( chatId =>
-        return Groups.AddToGroupAsync(Context.ConnectionId, "chatId").ContinueWith(_ => base.OnConnectedAsync());
+        var query = new GetAllPrivateChatsOfUser();
+        var chats = await Query(query);
+        chats.IfFailure(error => throw new HubException(error.ToString()));
+        await chats.IfSuccessAsync(async chats =>
+        {
+            foreach (var chat in chats)
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, chat.Id.ToString());
+            }
+        });
+        await base.OnConnectedAsync();
     }
 
     public override Task OnDisconnectedAsync(Exception exception)
     {
-        // (getChatIds for Context.ConnectionId).ForEach( chatId =>
-        return Groups.RemoveFromGroupAsync(Context.ConnectionId, "chatId").ContinueWith(_ => base.OnDisconnectedAsync(exception));
+        return base.OnDisconnectedAsync(exception);
     }
 
     [HubMethodName("chat.createWith")]
-    public Task CreateChatWith(Guid userId)
+    public async Task CreateChatWith(Guid userId)
     {
-        throw new NotImplementedException();
+        var command = new CreatePrivateChat.Command(userId);
+        var res = await Command(command);
+        await res.IfFailureAsync(SendError);
+        await res.IfSuccessAsync(async chat =>
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, chat.Id.ToString());
+            await Clients.Users(chat.Members.Select(g => g.ToString())).SendAsync("chat.created", Mapper.Map<PrivateChatDto>(chat));
+        });
+    }
+
+    [HubMethodName("chat.join")]
+    public async Task JoinChat(Guid chatId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, chatId.ToString());
     }
 
     [HubMethodName("chat.delete")]
-    public Task DeleteChat(Guid chatId)
+    public async Task DeleteChat(Guid chatId)
     {
-        throw new NotImplementedException();
+        var command = new DeletePrivateChat.Command(chatId);
+        var res = await Command(command);
+        await res.IfFailureAsync(SendError);
+        await res.IfSuccessAsync(async chat =>
+        {
+            // TODO: remove each member from group
+        });
     }
 
     [HubMethodName("message.send")]
-    public Task SendMessage(Guid chatId, string text)
+    public async Task SendMessage(Guid chatId, string text)
     {
-        throw new NotImplementedException();
-
-        // chatId.ToString()
-        // return Clients.Group("chatId").SendAsync("ReceiveMessage", text); // TODO: create object MessageOutput
+        var command = new SendPrivateMessage.Command(chatId, text);
+        var res = await Command(command);
+        await res.IfFailureAsync(SendError);
+        await res.IfSuccessAsync(async message =>
+        {
+            await Clients.OthersInGroup(message.ChatId.ToString()).SendAsync("message.received", Mapper.Map<PrivateMessageDto>(message));
+        });
     }
 
     [HubMethodName("message.edit")]
-    public Task EditMessage(Guid messageId, string text)
+    public async Task EditMessage(Guid messageId, string text)
     {
-        throw new NotImplementedException();
+        var command = new EditPrivateMessage.Command(messageId, text);
+        var res = await Command(command);
+        await res.IfFailureAsync(SendError);
+        await res.IfSuccessAsync(async message =>
+        {
+            await Clients.OthersInGroup(message.ChatId.ToString()).SendAsync("message.edited", Mapper.Map<PrivateMessageDto>(message));
+        });
+    }
 
-        // return Clients.Group("chatId").SendAsync("UpdateMessage", text); // TODO: create object MessageOutput
+    [HubMethodName("message.view")]
+    public async Task ViewMessage(Guid messageId)
+    {
+        var command = new ViewPrivateMessage.Command(messageId);
+        var res = await Command(command);
+        await res.IfFailureAsync(SendError);
+        await res.IfSuccessAsync(async message =>
+        {
+            await Clients.OthersInGroup(message.ChatId.ToString()).SendAsync("message.viewed", Mapper.Map<PrivateMessageDto>(message));
+        });
     }
 
     [HubMethodName("message.delete")]
-    public Task DeleteMessage(Guid messageId)
+    public async Task DeleteMessage(Guid messageId)
     {
-        throw new NotImplementedException();
-
-        // return Clients.Group("chatId").SendAsync("DeleteMessage", messageId.ToString()); // TODO: create object MessageOutput
+        var command = new DeletePrivateMessage.Command(messageId);
+        var res = await Command(command);
+        await res.IfFailureAsync(SendError);
+        await res.IfSuccessAsync(async message =>
+        {
+            await Clients.OthersInGroup(message.ChatId.ToString()).SendAsync("message.deleted", Mapper.Map<PrivateMessageDto>(message));
+        });
     }
 }
