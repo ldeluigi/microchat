e4df31ff589089018bab1ed0d9a823abb5756582
Simone <Simone.magnani4@studio.unibo.it>
2022-02-07 23:58:37 +0100

    Frontend uniform LoggedUser with Dto

--- frontend/microchat-frontend/src/model/LoggedUser.ts
+++ frontend/microchat-frontend/src/model/LoggedUser.ts
@@ -1,7 +1,8 @@
 export interface LoggedUser {
-  userId: string;
-  accessToken: string;
-  refreshToken: string;
+  userId: string,
+  accessToken: string,
+  refreshToken: string,
+  expirationDate: string
 }
 
 export interface AuthUserInfo {
