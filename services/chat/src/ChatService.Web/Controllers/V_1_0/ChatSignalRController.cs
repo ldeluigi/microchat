c7afb7d9f472a5711cacd904da367cde012f72e8
Simone <Simone.magnani4@studio.unibo.it>
2022-02-07 09:19:31 +0100

    Define frontend methods' name

--- services/chat/src/ChatService.Web/Controllers/V_1_0/ChatSignalRController.cs
+++ services/chat/src/ChatService.Web/Controllers/V_1_0/ChatSignalRController.cs
@@ -34,19 +34,22 @@ public class ChatSignalRController : AbstractMediatrHub
     [HubMethodName("message.send")]
     public Task SendMessage(Guid chatId, string text)
     {
+        throw new NotImplementedException();
         // chatId.ToString()
-        return Clients.Group("chatId").SendAsync(text);
+        return Clients.Group("chatId").SendAsync("ReceiveMessage", text); // TODO: create object MessageOutput
     }
 
     [HubMethodName("message.edit")]
     public Task EditMessage(Guid messageId, string text)
     {
         throw new NotImplementedException();
+        return Clients.Group("chatId").SendAsync("UpdateMessage", text); // TODO: create object MessageOutput
     }
 
     [HubMethodName("message.delete")]
     public Task DeleteMessage(Guid messageId)
     {
         throw new NotImplementedException();
+        return Clients.Group("chatId").SendAsync("DeleteMessage", messageId.ToString()); // TODO: create object MessageOutput
     }
 }
