ba9fb27273321ce7a7936e08ed1f55a60d2f7460
Simone <Simone.magnani4@studio.unibo.it>
2022-02-07 23:57:50 +0100

    Frontend add mapping for messages

--- frontend/microchat-frontend/src/model/Message.ts
+++ frontend/microchat-frontend/src/model/Message.ts
@@ -7,3 +7,26 @@ export interface Message {
   viewed: boolean,
   sender: string,
 }
+
+export interface MessageDto {
+  id: string,
+  chatId: string,
+  text: string,
+  sendTime: string,
+  lastEditTime: string,
+  viewed: boolean,
+  senderId: string
+}
+
+export function toMessage(dto: MessageDto): Message {
+  const message: Message = {
+    id: dto.id,
+    chatId: dto.chatId,
+    text: dto.text,
+    sendTime: new Date(Date.parse(dto.sendTime)),
+    edited: dto.lastEditTime? true : false,
+    viewed: dto.viewed,
+    sender: dto.senderId,
+  }
+  return message;
+}
