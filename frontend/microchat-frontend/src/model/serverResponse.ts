7276eeff84676740769a94ffe60b92c9f46d94f3
Simone <Simone.magnani4@studio.unibo.it>
2022-02-08 13:14:31 +0100

    Frontend add paginate response

--- frontend/microchat-frontend/src/model/serverResponse.ts
+++ frontend/microchat-frontend/src/model/serverResponse.ts
@@ -1,3 +1,12 @@
 export interface Response<DataType> {
   data: DataType;
 }
+
+export interface ResponsePaginate<DataType> extends Response<DataType> {
+  meta: {
+    pageIndex: number,
+    pageSize: number,
+    rowCount: number,
+    pageCount: number
+  },
+}
