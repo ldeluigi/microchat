98512eb1f689f2a8941fbf63d0c7d6baf43d5140
Luca Deluigi <lucadelu97@gmail.com>
2022-02-08 10:31:54 +0100

    Remove restriction of GetUser

--- services/user/src/UserService.Infrastructure/DataAccess/Queries/GetUserQueryHandler.cs
+++ services/user/src/UserService.Infrastructure/DataAccess/Queries/GetUserQueryHandler.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
-using EasyDesk.CleanArchitecture.Application.Authorization;
 using EasyDesk.CleanArchitecture.Application.ErrorManagement;
 using EasyDesk.CleanArchitecture.Application.Mediator;
 using EasyDesk.CleanArchitecture.Application.Responses;
@@ -8,7 +7,6 @@ using EasyDesk.CleanArchitecture.Dal.EfCore.Utils;
 using Microchat.UserService.Application.Queries;
 using System.Linq;
 using System.Threading.Tasks;
-using UserService.Application;
 
 namespace UserService.Infrastructure.DataAccess.Queries;
 
@@ -16,21 +14,15 @@ public class GetUserQueryHandler : RequestHandlerBase<GetUser.Query, UserOutput>
 {
     private readonly UserContext _userContext;
     private readonly IMapper _mapper;
-    private readonly IUserInfoProvider _userInfoProvider;
 
-    public GetUserQueryHandler(UserContext userContext, IMapper mapper, IUserInfoProvider userInfoProvider)
+    public GetUserQueryHandler(UserContext userContext, IMapper mapper)
     {
         _userContext = userContext;
         _mapper = mapper;
-        _userInfoProvider = userInfoProvider;
     }
 
     protected override async Task<Response<UserOutput>> Handle(GetUser.Query request)
     {
-        if (_userInfoProvider.RequireUserId() != request.UserId)
-        {
-            return ResponseImports.Failure<UserOutput>(new NotFoundError());
-        }
         return await _userContext
             .Users
             .Where(u => u.Id == request.UserId)
